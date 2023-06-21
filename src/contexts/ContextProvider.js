02145359aa5a3ba02082eaccbee14f9214c427fc
Sir-Rumeh <nosarumenimuentinyan@gmail.com>
2023-06-21 19:05:53 +0100

    redirect dashboard contexts

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -11,7 +11,7 @@ const StateContext = createContext();
 
 export const ContextProvider = ({ children }) => {
 	// const [screenSize, setScreenSize] = useState(undefined);
-	const [currentColor, setCurrentColor] = useState("#03C9D7");
+	// const [currentColor, setCurrentColor] = useState("#03C9D7");
 	const [currentMode, setCurrentMode] = useState("Light");
 	const [themeSettings, setThemeSettings] = useState(false);
 	const [activeMenu, setActiveMenu] = useState(true);
