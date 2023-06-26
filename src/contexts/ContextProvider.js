3b47046d14dc11d085d3cfce3d8a4c7786ff115e
Sir-Rumeh <nosarumenimuentinyan@gmail.com>
2023-06-26 17:47:55 +0100

    redirect dashboard contexts

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -39,7 +39,7 @@ export const ContextProvider = ({ children }) => {
 				// activeMenu,
 				// screenSize,
 				// setScreenSize,
-				handleClick,
+				// handleClick,
 				handleClose,
 				isClicked,
 				initialState,
