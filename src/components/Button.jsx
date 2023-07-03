41d5f37769257aacf1d6d2f0716c2bc2ebc20d95
Sir-Rumeh <nosarumenimuentinyan@gmail.com>
2023-07-03 18:37:36 +0100

    refactor button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 // import { useStateContext } from "../contexts/ContextProvider";
 
 const Button = ({ icon, bgColor, color, bgHoverColor, size, text, borderRadius, width }) => {
-	const { setIsClicked, initialState } = useStateContext();
+	// const { setIsClicked, initialState } = useStateContext();
 
 	return (
 		<button
