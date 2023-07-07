ec79e6ada0984507c991698d0697d8034e543df6
Sir-Rumeh <nosarumenimuentinyan@gmail.com>
2023-07-07 18:28:20 +0100

    refactor chat

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -17,13 +17,13 @@ const Chat = () => {
 						5 New
 					</button> */}
 				</div>
-				<Button
+				{/* <Button
 					icon={<MdOutlineCancel />}
 					color="rgb(153, 171, 180)"
 					bgHoverColor="light-gray"
 					size="2xl"
 					borderRadius="50%"
-				/>
+				/> */}
 			</div>
 			<div className="mt-5 ">
 				{chatData?.map((item, index) => (
